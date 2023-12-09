a9592e996e0743a7f3613105e1ed8ac61a2526ce
aten88 <virtuoso_ten@mail.ru>
2023-12-09 18:02:07 +0300

    Добавил в settings правила Feeds

--- training_scrapy/spiders/example.py
+++ training_scrapy/spiders/example.py
@@ -1,5 +1,7 @@
 import scrapy
 
+from training_scrapy.items import QuoteItem
+
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
@@ -7,11 +9,12 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         for quote in response.css('div.quote'):
-            yield {
+            data = {
                 'text': quote.css('span.text::text').get(),
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('a.tag::text').getall()
             }
+            yield QuoteItem(data)
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
